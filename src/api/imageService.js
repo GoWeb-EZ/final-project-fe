db03257aeb5aaaaf4762c7666702e3a62f667acc
Yooshin Kim <yusinkim.or@gmail.com>
2024-05-31 01:42:38 +0900

    feat : #3 - huggingFace 정보 env로 수정

--- src/api/imageService.js
+++ src/api/imageService.js
@@ -1,43 +1,43 @@
 import axios from "axios";
 import { Buffer } from "buffer";
 
-const API_TOKEN = "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";
+const API_URL = process.env.REACT_APP_HUGGINGFACE_API_URL;
+const API_TOKEN = process.env.REACT_APP_HUGGINGFACE_API_KEY;
+
 export const convertImg = (text) => {
-  const inputData = {
-    inputs: `doodling-ai2, ${text}`,
-    options: {
-      wait_for_model: true,
-      use_cache: false,
-    },
-  };
+    const inputData = {
+        inputs: `doodling-ai2, ${text}`,
+        options: {
+            wait_for_model: true,
+            use_cache: false,
+        },
+    };
 
-  return new Promise((resolve, reject) => {
-    const url = `https://api-inference.huggingface.co/models/Serena47/doodling-ai2`;
+    return new Promise((resolve, reject) => {
+        axios({
+            url: API_URL,
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            data: JSON.stringify(inputData),
+            responseType: "arraybuffer",
+        })
+            .then((resp) => {
+                console.log(text);
 
-    axios({
-      url: url,
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${API_TOKEN}`,
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      data: JSON.stringify(inputData),
-      responseType: "arraybuffer",
-    })
-      .then((resp) => {
-        console.log(text);
+                const mimeType = resp.headers["content-type"];
+                const result = resp.data;
+                const base64data = Buffer.from(result).toString("base64");
+                const img = `data:${mimeType};base64,` + base64data;
 
-        const mimeType = resp.headers["content-type"];
-        const result = resp.data;
-        const base64data = Buffer.from(result).toString("base64");
-        const img = `data:${mimeType};base64,` + base64data;
-
-        resolve(img);
-      })
-      .catch((e) => {
-        console.log(e);
-        reject(e);
-      });
-  });
+                resolve(img);
+            })
+            .catch((e) => {
+                console.log(e);
+                reject(e);
+            });
+    });
 };
