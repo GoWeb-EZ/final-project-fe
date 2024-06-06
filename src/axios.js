1052574fb5488a1f85cba8935e18c61172fe7ec2
Yooshin Kim <yusinkim.or@gmail.com>
2024-06-07 02:42:42 +0900

    feat: 백 주소 https로 변경

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://20.196.96.170:8080/api",
+    baseURL: "https://20.196.96.170:8080/api",
 });
 
 API.interceptors.request.use(
