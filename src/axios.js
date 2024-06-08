92f3e0fbf592eba08d1a1177a631dcd1fd96d48c
Yooshin Kim <yusinkim.or@gmail.com>
2024-06-08 16:12:46 +0900

    feat : 백 API 주소 변경

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://20.196.96.170:8080/api",
+    baseURL: "https://goweb10be.store/api",
 });
 
 API.interceptors.request.use(
