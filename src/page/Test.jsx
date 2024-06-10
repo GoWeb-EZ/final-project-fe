558c0b9a82058f7d05df861dfb7c42f5e2efc111
Yooshin Kim <yusinkim.or@gmail.com>
2024-06-10 15:48:38 +0900

    Feature/#14-team (#15)
    * feat : 팀 페이지 이미지 및 이름 수정

--- src/page/Test.jsx
+++ src/page/Test.jsx
@@ -1,22 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 
 export default function Test() {
-  return (
-    <>
-      <TextArea
-        type="text"
-        onMouseUp={(e) => {
-          console.log(e);
-          console.log(window.getSelection().toString());
-        }}
-      />
-    </>
-  );
+    return (
+        <>
+            <TextArea
+                type="text"
+                onMouseUp={(e) => {
+                    console.log(e);
+                    console.log(window.getSelection().toString());
+                }}
+            />
+        </>
+    );
 }
 
 const TextArea = styled.input`
-  width: 300px;
-  height: 200px;
-  border: 1px solid red;
+    width: 300px;
+    height: 200px;
+    border: 1px solid red;
 `;
